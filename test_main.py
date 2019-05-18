f4b15d6c2b391dd953b7444f9d9ab0de0a78f872
Alonso Enrique <alons45@gmail.com>
2019-05-18 10:59:51 -0500

    test bouncy_number method refactor another methods

--- test_main.py
+++ test_main.py
@@ -1,9 +1,10 @@
-import unittest
 import random
 
-from main import find_percent_proportion, increasing_number, decreasing_number, convert_integer_to_list
+from unittest import TestCase, mock
 
-class BouncyNumberTestCase(unittest.TestCase):
+from main import find_percent_proportion, increasing_number, decreasing_number, convert_integer_to_list, bouncy_number
+
+class BouncyNumberTestCase(TestCase):
     def test_fifty_percent_proportion(self):
         self.assertEqual(find_percent_proportion(50)[0], 538)
 
@@ -18,11 +19,26 @@ class BouncyNumberTestCase(unittest.TestCase):
         self.assertTrue(decreasing_number(convert_integer_to_list(9933)))
         self.assertFalse(decreasing_number(convert_integer_to_list(76326)))
 
-    def test_convert_in_to_list(self):
+    @mock.patch('main.decreasing_number', autospec=True)
+    @mock.patch('main.increasing_number', autospec=True)
+    def test_bouncy_number(self, mock_increasing_number, mock_decreasing_number):
+        mock_increasing_number.return_value = False
+        mock_decreasing_number.return_value = False
+
+        self.assertTrue(bouncy_number([1, 4, 5]))
+        self.assertTrue(mock_increasing_number.called)
+        self.assertTrue(mock_decreasing_number.called)
+
+        mock_increasing_number.return_value = True
+
+        self.assertFalse(bouncy_number([1, 4, 5]))
+        self.assertTrue(mock_increasing_number.called)
+
+    def test_convert_integer_to_list(self):
         self.assertEqual(list, type(convert_integer_to_list(45435)))
 
-    def test_types_convert_in_to_list(self):
+    def test_types_convert_integer_to_list(self):
         self.assertRaises(TypeError, convert_integer_to_list, 'dsfsf')
 
-    def test_values_increasing_number(self):
+    def test_values_convert_integer_to_list(self):
         self.assertRaises(ValueError, convert_integer_to_list, -19293)
